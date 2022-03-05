37f80eb8fe7c84f5eb3ed3dc3938402f543be948
Piirainen Samuel <piirais1@whitespace.org.aalto.fi>
2022-03-05 21:01:08 +0200

    ...

--- refine_glnn_train.py
+++ refine_glnn_train.py
@@ -12,7 +12,10 @@ from pathlib import Path
 
 from utils.dataset import get_datasets
 from explainers import *
+from gnns import *
 from glnns.glnn import GLNN
+from datasets.ba3motif_dataset import BA3Motif
+from torch_scatter import scatter
 from torch_geometric.data import DataLoader
 
 
@@ -47,47 +50,54 @@ args = parse_args()
 
 device = torch.device(f"cuda:{args.cuda}" if torch.cuda.is_available() else "cpu")
 
-_, _, test_dataset = get_datasets(name=args.dataset)
+#test_dataset = BA3Motif(args.data_path, mode='testing')
+#val_dataset = BA3Motif(args.data_path, mode='evaluation')
+train_dataset = BA3Motif('data/BA3', mode='training')
 
-graph_mask_path = f"param/filtered/{args.dataset}_idx_test.pt"
-graph_mask = torch.load(graph_mask_path)
-test_loader = DataLoader(
-    test_dataset[graph_mask], batch_size=1, shuffle=False, drop_last=False
-)
+#test_loader = DataLoader(test_dataset,
+#                         batch_size=args.batch_size,
+#                         shuffle=False
+#                         )
+#val_loader = DataLoader(val_dataset,
+#                        batch_size=args.batch_size,
+#                        shuffle=False
+#                        )
+train_loader = DataLoader(train_dataset,
+                          batch_size=128,
+                          shuffle=True
+                          )
+model_path = f"param/gnns/{args.dataset}_net.pt"
+model = torch.load(model_path, map_location=device)
 
-
-gnns_dict = {
-    "mutag": "mutag_gnn",
-    "ba3": "ba3motif_gnn",
-    "mnist": "mnist_gnn",
-    "vg": "vg_gnn",
-}
-refine_model_path = f"gnns/{gnns_dict[args.dataset]}.py"
-refine = torch.load(refine_model_path)
-refine.remap_device(device)
-
-# ---------------------------------------------------
-in_features = torch.flatten(test_dataset[0].x, 1, -1).size(1)
-mlp_model = GLNN(device=device, in_features=in_features, out_features=1)
+in_features = torch.flatten(train_dataset[0].x, 1, -1).size(1)
+mlp_model = GLNN(device=device, in_features=in_features, out_features=3)
 loss_label = torch.nn.CrossEntropyLoss()
 loss_teacher = torch.nn.KLDivLoss()
 loss_lambda = 0.5
 optimizer = torch.optim.Adam(mlp_model.parameters(), lr=1e-4)
-print("Evaluate ReFine w.r.t. ACC-AUC...")
 for epoch in range(10):
     running_loss = 0.0
     it = 0
-    for g in tqdm(iter(test_loader), total=len(test_loader)):
+    for g in tqdm(iter(train_loader), total=len(train_loader)):
         g.to(device)
-        z = refine.explain_graph(
-            g, fine_tune=True, to_numpy=False, ratio=0.5, lr=args.lr, epoch=args.epoch
-        )
+        with torch.no_grad():
+            z = model(g.x, g.edge_index, g.edge_attr, g.batch)
 
         optimizer.zero_grad()
-        y_hat = mlp_model(g.x)
-        loss = loss_lambda * loss_label(y_hat, g.y) + (1 - loss_lambda) * loss_teacher(
-            y_hat, z
-        )
+        mlp_in = scatter(g.x, g.batch, dim=0, dim_size=int(g.batch.max().item() + 1), reduce='add')
+        y_hat = mlp_model(mlp_in)
+        #print(f"g.x.shape: {g.x.shape}")
+        #print(f"g.batch.shape: {g.batch.shape}")
+        #print(f"g.y.shape: {g.y.shape}")
+        #print(f"y_hat.shape: {y_hat.shape}")
+        #print(f"z.shape: {z.shape}")
+        #g.x.shape: torch.Size([2737, 4])
+        #g.batch.shape: torch.Size([2737])
+        #g.y.shape: torch.Size([128])
+        #y_hat.shape: torch.Size([128, 3])
+        #z.shape: torch.Size([128, 3])
+        loss = loss_lambda * loss_label(y_hat, g.y) \
+                + (1 - loss_lambda) * loss_teacher(y_hat, z)
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
